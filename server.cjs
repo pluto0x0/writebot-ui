a739f01640ae5e8bb6f9720d93fe767525139aad
Zifan Ying <yzzzf0@gmail.com>
2025-05-20 11:08:39 +0800

    123

--- server.cjs
+++ server.cjs
@@ -53,6 +53,6 @@ io.on('connection', socket => {
 })
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
+server.listen(PORT, '0.0.0.0', () =>
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`)
 )
